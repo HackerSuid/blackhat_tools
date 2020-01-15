43a1d5d948052c17c0995e327cbab48d26ff4b7b
HackerSuid <hackersuid@gmail.com>
2020-01-15 09:04:17 -0600

    updates

--- lan_discover.py
+++ lan_discover.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 
+'''
+After intruding into a (w)lan, this script
+automates scanning the network for devices and
+identifying what they are.
+'''
+
 import arpreq
 import ipaddress
+import json
 from mac_vendor_lookup import MacLookup
 import netifaces
+import nmap
+import pprint
 import sys
+
+'''
+Need to run macchanger before scanning the network.
+'''
+
+hosts_info = {}
 
 def_gate = netifaces.gateways()['default'][netifaces.AF_INET]
 dev = def_gate[1]
@@ -16,17 +31,29 @@ network = ipaddress.ip_network("%s/%s" % (addr, netmask), strict=False)
 
 start = network.network_address+1
 end = network.broadcast_address
-print("ARP scanning network %s (%s - %s)" %
-    (network.network_address, start, end-1))
 
-print("Downloading OUI list...")
 maclookup = MacLookup()
+nmap = nmap.PortScanner()
 
-for ipint in range(int(start), int(end)):
+#for ipint in range(int(start), int(end)):
+for ipint in range(int(start), int(start)+1):
     ip = ipaddress.IPv4Address(ipint)
+    ipstr = str(ip)
+    hosts_info[ipstr] = {}
     mac = arpreq.arpreq(ip)
+    vendor = None
+
     if mac is not None:
         vendor = maclookup.lookup(mac)
-        print("\t%s %s (%s)" % (ip, mac, vendor))
-    else:
-        print("\t%s <no host>" % ip)
+        scan_result = {}
+        nmap.scan(hosts=ipstr, ports='1-1000', arguments='-sS -O')
+        scan_result['os'] = nmap[ipstr]['osmatch'][0]['name']
+        scan_result['open_tcp'] = [p for p in nmap[ipstr]['tcp']]
+        #nmap.scan(hosts=ipstr, ports='1-1000', arguments='-sU')
+        #print(nmap[ipstr]['udp'])
+
+    hardware_info = [mac, vendor]
+    hosts_info[ipstr]['hardware'] = hardware_info
+    hosts_info[ipstr]['scan_result'] = scan_result
+
+pprint.pprint(hosts_info)
